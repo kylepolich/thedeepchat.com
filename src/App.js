70004783f7e404ba04e69a897dca5b7bd71ffbac
Kyle Polich <kyle@dataskeptic.com>
2021-12-05 08:31:18 -0800

    setup

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>The Deep Chat</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
