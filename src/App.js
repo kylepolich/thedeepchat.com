5a77585c004ccc4efd73f9dc81a2b1e1b7143501
Kyle Polich <kyle@dataskeptic.com>
2021-12-05 08:33:54 -0800

    youtube

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>The Deep Chat</p>
+        <a href="https://www.youtube.com/channel/UCn-rnnQT5q5nNE1JnsvFXow">Youtube</a>
       </header>
     </div>
   );
